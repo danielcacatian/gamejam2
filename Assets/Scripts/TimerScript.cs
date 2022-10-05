6d63c5d6e49f2246583c5617e5c59270aeaa559f
danielcacatian <70924068+danielcacatian@users.noreply.github.com>
2022-10-05 17:21:59 -0400

    Launched Game Jam 2

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public TMP_Text TimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerOn)
+        {
+            if(TimeLeft < 720)
+            {
+                TimeLeft += Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        TimerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
